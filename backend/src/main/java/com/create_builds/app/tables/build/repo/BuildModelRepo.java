082ce290c6477440b9fb5af7be5c77df05ae8725
kaedeng <kdeng@mines.edu>
2024-12-31 22:03:01 -0700

    Added Annotations alongside a property line
    Does not work locally (likely because of AWS firewall, will test on PR. If it doesn't work, then delete branch)

--- backend/src/main/java/com/create_builds/app/tables/build/repo/BuildModelRepo.java
+++ backend/src/main/java/com/create_builds/app/tables/build/repo/BuildModelRepo.java
@@ -1,8 +1,8 @@
 package com.create_builds.app.tables.build.repo;
 
 import org.springframework.data.repository.CrudRepository;
-
 import com.create_builds.app.tables.build.model.BuildModel;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface BuildModelRepo extends CrudRepository<BuildModel, Integer>{
 }
--- backend/src/main/java/com/create_builds/app/tables/upvotes/repo/UpvoteModelRepo.java
+++ backend/src/main/java/com/create_builds/app/tables/upvotes/repo/UpvoteModelRepo.java
@@ -2,6 +2,7 @@ package com.create_builds.app.tables.upvotes.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import com.create_builds.app.tables.upvotes.model.UpvoteModel;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface UpvoteModelRepo extends CrudRepository<UpvoteModel, Integer>{
 }
