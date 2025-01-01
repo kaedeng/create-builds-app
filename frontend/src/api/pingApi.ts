6fe3c6ec02768e987b900f7de7b6fb789be8fb31
Swiffels <swp9gaming@gmail.com>
2025-01-01 15:37:12 -0700

    Update config java and Axios instance

--- frontend/src/api/pingApi.ts
+++ frontend/src/api/pingApi.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const PingApi = async () => {
   try {
     const response = await axios.get(
-      'cba-backend-lb-1990243648.us-west-2.elb.amazonaws.com/api/profile/ping'
+      'https://cba-backend-lb-1990243648.us-west-2.elb.amazonaws.com/api/profile/ping'
     );
     return response.data;
   } catch (error) {
