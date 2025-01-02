6fa19918592ee755e0502c82bd9fde62d8ee9c1d
Swiffels <swp9gaming@gmail.com>
2025-01-01 19:36:13 -0700

    Update SecurityConfig.java

--- backend/src/main/java/com/create_builds/app/authcontroller/SecurityConfig.java
+++ backend/src/main/java/com/create_builds/app/authcontroller/SecurityConfig.java
@@ -7,21 +7,41 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		
-		http
-			.authorizeHttpRequests(auth -> auth
-					.requestMatchers(HttpMethod.GET, "/", "/homepage-builds", "/builds/**", "/health/ping").permitAll()
-					.anyRequest().authenticated())
-			.oauth2Login(Customizer.withDefaults())
-			.requiresChannel(channel -> channel.anyRequest().requiresSecure());
-		return http.build();
-	}
-	
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers(HttpMethod.GET, "/", "/homepage-builds", "/builds/**", "/health/ping").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .requiresChannel(channel -> channel.anyRequest().requiresSecure())
+                .csrf(csrf -> csrf.disable());
+        
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://createbuildsmc.com"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
