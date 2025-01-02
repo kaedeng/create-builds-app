7dc7f3ada35bbe33320049580a2d26d7443827a5
Swiffels <swp9gaming@gmail.com>
2025-01-02 02:23:21 -0700

    test8

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './app/App';
 import reportWebVitals from './utils/reportWebVitals';
