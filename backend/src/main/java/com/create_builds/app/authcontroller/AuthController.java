d2654d20a3c0ef7ebfa055a49d1c52558e944238
Swiffels <swp9gaming@gmail.com>
2025-01-01 18:40:00 -0700

    Add security config and updates

---
+++ backend/src/main/java/com/create_builds/app/authcontroller/AuthController.java
@@ -0,0 +1,14 @@
+package com.create_builds.app.authcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+	@GetMapping("/api/auth")
+	public String authorizeLogin() {
+		return "redirect:/oauth2/authorization/google";
+	}
+	
+}
