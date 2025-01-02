1003d05bb64ae93aa342b0cd9d62a21c7097d121
Swiffels <swp9gaming@gmail.com>
2025-01-02 15:46:52 -0700

    test10

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,14 +1,14 @@
 import axiosInstance from './axiosInstance';
 
-// export const loginUser = async (idToken: string): Promise<string> => {
-//   try {
-//     const response = await axiosInstance.post('/auth', { idToken });
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error:', error);
-//     throw error;
-//   }
-// };
+export const loginUser = async (idToken: string): Promise<string> => {
+  try {
+    const response = await axiosInstance.post('/auth', { idToken });
+    return response.data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+};
 
 export const logoutUser = async (): Promise<string> => {
   try {
