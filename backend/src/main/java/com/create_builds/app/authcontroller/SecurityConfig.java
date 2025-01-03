369d705dd2e15d63ff39dd9a6a02b99d23788738
Swiffels <swp9gaming@gmail.com>
2025-01-03 00:14:03 -0700

    idkanymrpoe

--- backend/src/main/java/com/create_builds/app/authcontroller/SecurityConfig.java
+++ backend/src/main/java/com/create_builds/app/authcontroller/SecurityConfig.java
@@ -1,110 +1,66 @@
 package com.create_builds.app.authcontroller;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-
-import org.springframework.http.HttpMethod;
-
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.ForwardedHeaderFilter;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//
-//import java.util.List;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .cors(Customizer.withDefaults())
-//                .authorizeHttpRequests(auth -> auth
-//                                .requestMatchers(HttpMethod.GET, "/", "/api/homepage-builds", "/api/builds/**", "/api/health/ping").permitAll()
-//                                .requestMatchers("/api/login").authenticated()
-//                                .anyRequest().authenticated()
-//                )
-//                .oauth2Login(oauth2Login -> 
-//                oauth2Login
-//	                .successHandler((request, response, authentication) -> {
-//	                    OAuth2User user = (OAuth2User) authentication.getPrincipal();
-//	                    System.out.println("User details: " + user.getAttributes());
-//	                    response.sendRedirect("https://createbuildsmc.com");
-//	                })
-//                    .failureHandler((request, response, exception) -> {
-//                        exception.printStackTrace();
-//                    })
-//                )
-//                .csrf(csrf -> csrf.disable());
-//        
-//        return http.build();
-//    }
-//
-//
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("https://createbuildsmc.com", "https://api.createbuildsmc.com"));
-//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        configuration.setAllowedHeaders(List.of("*"));
-//        configuration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-//    
-//    @Bean
-//    public ForwardedHeaderFilter forwardedHeaderFilter() {
-//        return new ForwardedHeaderFilter();
-//    }
-//
-//}
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		
-		http
-		
-			.authorizeHttpRequests(auth -> {
-                auth.requestMatchers(HttpMethod.GET, "/", "/api/homepage-builds", "/api/builds/**", "/api/health/ping", "/login", "/oauth2/**", "/favicon.ico").permitAll();
-                auth.requestMatchers("/oauth2/authorization/**").permitAll();
-                auth.anyRequest().authenticated();
-			})
-			.oauth2Login(oauth2 -> oauth2
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers(HttpMethod.GET, "/", "/api/homepage-builds", "/api/builds/**", "/api/health/ping").permitAll()
+                                .requestMatchers("/api/login").authenticated()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login -> 
+                oauth2Login
 	                .successHandler((request, response, authentication) -> {
+	                    OAuth2User user = (OAuth2User) authentication.getPrincipal();
+	                    System.out.println("User details: " + user.getAttributes());
 	                    response.sendRedirect("https://createbuildsmc.com");
-	            })
-	        );
-		
-		return http.build();
-	}
-	
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("https://createbuildsmc.com")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowCredentials(true)
-            .allowedHeaders("*");
+	                })
+                    .failureHandler((request, response, exception) -> {
+                        exception.printStackTrace();
+                    })
+                )
+                .csrf(csrf -> csrf.disable());
+        
+        return http.build();
     }
-	
-}
 
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://createbuildsmc.com", "https://api.createbuildsmc.com"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
+
+}
