33a0367dcae761998d04a0ce4c81826dfcb95d25
Swiffels <swp9gaming@gmail.com>
2025-01-04 05:43:46 -0700

    Update pingApi.ts

--- frontend/src/api/pingApi.ts
+++ frontend/src/api/pingApi.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const PingApi = async () => {
   try {
     const response = await axios.get(
-      'cba-lb-1126998897.us-west-2.elb.amazonaws.com/api/health/ping',
+      'https://createbuildsmc.com/api/health/ping',
       { withCredentials: true }
     );
     return response.data;
