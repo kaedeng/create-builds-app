f33455eea2fe9056e7b5719d452273ec156bcd82
Swiffels <swp9gaming@gmail.com>
2025-01-04 14:18:38 -0700

    Update SecurityConfig.java

--- backend/src/main/java/com/create_builds/app/authcontroller/SecurityConfig.java
+++ backend/src/main/java/com/create_builds/app/authcontroller/SecurityConfig.java
@@ -1,71 +1,3 @@
-//package com.create_builds.app.authcontroller;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.ForwardedHeaderFilter;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//
-//import java.util.List;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//	@Bean
-//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//	    http
-//	            .cors(Customizer.withDefaults())
-//	            .authorizeHttpRequests(auth -> auth
-//	                            .requestMatchers(HttpMethod.GET, "/", "/api/homepage-builds", "/api/builds/**", "/api/health/ping").permitAll()
-//	                            .requestMatchers("/api/login/**").permitAll()
-//	                            .anyRequest().authenticated()
-//	            )
-//	            .oauth2Login(oauth2Login -> 
-//	            oauth2Login
-//	                .successHandler((request, response, authentication) -> {
-//	                    OAuth2User user = (OAuth2User) authentication.getPrincipal();
-//	                    System.out.println("User details: " + user.getAttributes());
-//	                    response.sendRedirect("https://createbuildsmc.com");
-//	                })
-//	                .failureHandler((request, response, exception) -> {
-//	                    exception.printStackTrace();
-//	                })
-//	            )
-//	            .csrf(csrf -> csrf.disable())
-//	            .requiresChannel(channel -> channel
-//	                    .anyRequest().requiresSecure());
-//	    return http.build();
-//	}
-//
-//
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("https://createbuildsmc.com", "https://api.createbuildsmc.com"));
-//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        configuration.setAllowedHeaders(List.of("*"));
-//        configuration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-//    
-//    @Bean
-//    public ForwardedHeaderFilter forwardedHeaderFilter() {
-//        return new ForwardedHeaderFilter();
-//    }
-//
-//}
-
 package com.create_builds.app.authcontroller;
 
 import org.springframework.context.annotation.Bean;
@@ -74,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -86,21 +19,32 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .cors(Customizer.withDefaults())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.GET, "/", "/api/homepage-builds", "/api/builds/**", "/api/health/ping").permitAll()
-                .requestMatchers("/api/login/**").permitAll()
-                .anyRequest().authenticated() // All other requests require authentication
-            )
-            .formLogin(Customizer.withDefaults()) // Enable form-based login with default settings
-            .csrf(csrf -> csrf.disable()) // Optional: Disable CSRF for simplicity
-            .requiresChannel(channel -> channel
-                .anyRequest().requiresSecure()); // Enforce HTTPS
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    http
+	            .cors(Customizer.withDefaults())
+	            .authorizeHttpRequests(auth -> auth
+	                            .requestMatchers(HttpMethod.GET, "/", "/api/homepage-builds", "/api/builds/**", "/api/health/ping").permitAll()
+	                            .requestMatchers("/api/login/**").permitAll()
+	                            .anyRequest().authenticated()
+	            )
+	            .oauth2Login(oauth2Login -> 
+	            oauth2Login
+	                .successHandler((request, response, authentication) -> {
+	                    OAuth2User user = (OAuth2User) authentication.getPrincipal();
+	                    System.out.println("User details: " + user.getAttributes());
+	                    response.sendRedirect("https://createbuildsmc.com");
+	                })
+	                .failureHandler((request, response, exception) -> {
+	                    exception.printStackTrace();
+	                })
+	            )
+	            .csrf(csrf -> csrf.disable())
+	            .requiresChannel(channel -> channel
+	                    .anyRequest().requiresSecure());
+	    return http.build();
+	}
+
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
@@ -114,9 +58,10 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
+    
     @Bean
     public ForwardedHeaderFilter forwardedHeaderFilter() {
         return new ForwardedHeaderFilter();
     }
+
 }
