2ca0b7de90357f2e0e0f5bfa299bb03df5faf8d0
Swiffels <swp9gaming@gmail.com>
2025-01-04 17:50:22 -0700

    test 1010100101110

--- backend/src/main/java/com/create_builds/app/authcontroller/SecurityConfig.java
+++ backend/src/main/java/com/create_builds/app/authcontroller/SecurityConfig.java
@@ -1,50 +1,6 @@
 package com.create_builds.app.authcontroller;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.ForwardedHeaderFilter;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//
-//import java.util.List;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//	@Bean
-//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//	    http
-//	            .cors(Customizer.withDefaults())
-//	            .authorizeHttpRequests(auth -> auth
-//	                            .requestMatchers(HttpMethod.GET, "/", "/api/homepage-builds", "/api/builds/**", "/api/health/ping").permitAll()
-//	                            .anyRequest().permitAll()
-//	            )
-//	            .oauth2Login(oauth2Login -> 
-//	            oauth2Login
-//	                .successHandler((request, response, authentication) -> {
-//	                    OAuth2User user = (OAuth2User) authentication.getPrincipal();
-//	                    System.out.println("User details: " + user.getAttributes());
-//	                    response.sendRedirect("https://createbuildsmc.com");
-//	                })
-//	                .failureHandler((request, response, exception) -> {
-//	                    exception.printStackTrace();
-//	                })
-//	            )
-//	            
-//	    return http.build();
-//	}
-//}
-
-	
-	import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 	import org.springframework.security.web.SecurityFilterChain;
@@ -56,7 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 	        http
 	            .authorizeHttpRequests(auth -> auth
-	                .anyRequest().authenticated() // All requests require authentication
+	                .anyRequest().authenticated() 
 	            )
 	            .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
 		        .csrf(csrf -> csrf.disable())
